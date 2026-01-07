36881fa2f7bafd7758123676b401843ecfe1db52
33TChaubey <chaubeytn2103@outlook.com>
2026-01-07 07:05:39 +0530

    DRY principle activated

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tv-e_i___515&r@q+8j+7#*f9f*zkqpiohj_ugj380rz$9zi*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.http import request, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blogs, Category
 
 
@@ -7,10 +7,11 @@ from .models import Blogs, Category
 def posts_by_category(request, category_id):
     #fetch the post that belongs to the category with the id
     posts = Blogs.objects.filter(status = 'Published',category=category_id)
-    try:
-        category = Category.objects.get(id=category_id)
-    except:
-        return redirect('home')
+    # try:
+    #     category = Category.objects.get(id=category_id)
+    # except:
+    #     return redirect('home')
+    category = get_object_or_404(Category, id = category_id)
 
     context = {'posts': posts, 'category': category}
     return render(request, 'posts_by_category.html', context)
